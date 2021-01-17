d6d614539ab965947dd5c1952d7bdea506377828
Dante Barba <dantebarba@gmail.com>
2021-01-17 20:59:10 -0300

    asd

--- src/qbt_api.py
+++ src/qbt_api.py
@@ -5,7 +5,9 @@ import os
 qbt_client = qbittorrentapi.Client(host=os.environ["QBT_HOST"], port=int(os.environ["QBT_PORT"]), username=os.environ["QBT_USERNAME"], password=os.environ["QBT_PASS"])
 
 def pause_all():
-    qbt_client.torrents.pause.all()
+    for torrent in qbt_client.torrents.info.active():
+        if torrent.progress < 100:
+            torrent.pause()
     
 def resume_all():
     for torrent in qbt_client.torrents.info.paused():
